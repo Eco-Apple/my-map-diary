d49eaba372c6f68944cb38194afce81c37768536
Myawmyaw <eco.villaraza@gmail.com>
2024-10-30 19:26:45 +0800

    init-fix: keyboard problem.

--- MyMapDiary/Views/Screens/LocationDetailScreen.swift
+++ MyMapDiary/Views/Screens/LocationDetailScreen.swift
@@ -6,43 +6,99 @@
 //
 
 import SwiftUI
+import Combine
 
 struct LocationDetailScreen: View {
+    @Environment(\.dismiss) var dismiss
     @State var location: Location
+    @State private var isKeyboardVisible = false
+    
+    let defaultHeight = 300.0
     
     var body: some View {
-        VStack(spacing: 0) {
-            ZStack {
+        ZStack {
+            VStack {
                 Image(uiImage: location.uiImage)
                     .resizable()
                     .scaledToFill()
-                
-                Color.black.opacity(0.3)
+                Spacer()
+                    .frame(height: defaultHeight + ( isKeyboardVisible ? 150 : 0))
             }
-            .frame(height: UIScreen.main.bounds.height * 0.6)
+            .ignoresSafeArea(.all)
             
-            Form {
-                Section {
-                    TextField("Title", text: $location.title)
+            VStack(spacing: .zero) {
+                Spacer()
+                VStack {
+                    Spacer()
+                    Form {
+                        
+                        Section {
+                            TextField("Title", text: $location.title)
+                        }
+                        
+                        Section {
+                            TextEditor(text: $location.message)
+                                .placeHolder("Share your thoughts or memories...", text: $location.message)
+                                .frame(height: 150)
+                        }
+                    }
+                    
+                    .listSectionSpacing(.compact)
+                    .scrollDisabled(true)
+                    .offset(y: -40)
                 }
-                
-                Section {
-                    TextEditor(text: $location.message)
-                        .placeHolder("Share your thoughts or memories...", text: $location.message)
-                        .frame(height: 150)
+                .frame(height: defaultHeight + ( isKeyboardVisible ? 150 : 0))
+                .padding(.top, 55)
+                .background(Color(.systemGroupedBackground))
+            }
+            .frame(width: UIScreen.main.bounds.width)
+            
+            VStack {
+                Spacer()
+                HStack {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Label("Back", systemImage: "chevron.backward")
+                    }
+                    .padding()
+                    
+                    Spacer()
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "trash")
+                    }
+                    .padding()
                 }
-            }
-            .listSectionSpacing(.compact)
-        }
-        .padding(.top, -100)
-        .navigationBarTitleDisplayMode(.inline)
-        .ignoresSafeArea(.all)
-        .scrollBounceBehavior(.basedOnSize)
-        .toolbar {
-            Button("Delete this location", systemImage: "trash") {
+                .frame(width: UIScreen.main.bounds.width)
+                .background(.clear)
                 
+                Spacer()
+                    .frame(height: defaultHeight + ( isKeyboardVisible ? 150 : 0))
             }
+            .frame(width: UIScreen.main.bounds.width)
         }
+        
+        .background(Color(.systemGroupedBackground))
+        .navigationBarHidden(true)
+        .onReceive(Publishers.keyboardVisible) { isVisible in
+            isKeyboardVisible = isVisible
+        }
+    }
+}
+
+
+// Keyboard visibility publisher
+extension Publishers {
+    static var keyboardVisible: AnyPublisher<Bool, Never> {
+        let keyboardWillShow = NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification)
+            .map { _ in true }
+        let keyboardWillHide = NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)
+            .map { _ in false }
+
+        return MergeMany(keyboardWillShow, keyboardWillHide)
+            .eraseToAnyPublisher()
     }
 }
 
